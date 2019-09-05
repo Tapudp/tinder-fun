f30e503e81a55b2b8de1a6ed4928297b42748d3e
Tapudp <divyeshpar2496@gmail.com>
2019-09-06 00:56:44 +0530

    Updates

---
+++ precache-manifest.5c0acde0bee4d681529c34d1ad5e9c01.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad18faf260c8089bbb238c88418121c",
+    "url": "/tinder-fun/index.html"
+  },
+  {
+    "revision": "c305f771565db328fdd6",
+    "url": "/tinder-fun/static/css/main.325a8820.chunk.css"
+  },
+  {
+    "revision": "bcf82c7443e1fd014a4d",
+    "url": "/tinder-fun/static/js/2.c84338a7.chunk.js"
+  },
+  {
+    "revision": "c305f771565db328fdd6",
+    "url": "/tinder-fun/static/js/main.7f1d3ec3.chunk.js"
+  },
+  {
+    "revision": "3094985f544042c39b79",
+    "url": "/tinder-fun/static/js/runtime~main.ef8056ac.js"
+  }
+]);
